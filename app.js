1adf5ee88d10107ae08cb4de85dda3a1692a4d3d
mirajshaikh <miraj@Mirajs-MacBook-Pro.local>
2020-10-11 17:11:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let shoppingList;
+let veggi = ['potato', 'tomato', 'lemon']
+let fruits = ['mango', 'apple', 'banana']
+shoppingList = veggi.concat(fruits)
+
+
+
+console.log(shoppingList);
+console.log(shoppingList.includes('mango'))
